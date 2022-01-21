5d511bc5d485ae21c14f5bd986421ff2a9cfc47e
Evgeniy <gavrilov-e@yandex.ru>
2022-01-21 09:12:33 +0300

    Для LightWeightCar изменен текст в консоли. В Main выводится в консоль ссылка на объект класса person.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Person p = new Person.Builder().addFName("Michael").addLName("Sunboy").addCountry("Neverland").addGender("male").addAge(30).build();
-        System.out.println(p.getClass());
+        System.out.println(p);
     }
 }
